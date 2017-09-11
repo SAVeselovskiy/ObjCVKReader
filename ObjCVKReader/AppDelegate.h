33fb91ab1360fa384bbafb0f1a4bb7dd838f40b6
SAVeselovskiy <veselovskiysergey94@gmail.com>
2017-09-11 21:33:29 +0300

    Initial Commit

---
+++ ObjCVKReader/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ObjCVKReader
+//
+//  Created by Сергей Веселовский on 11.09.17.
+//  Copyright © 2017 Сергей Веселовский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
